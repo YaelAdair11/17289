93f961f4253c576c6f5cfa63e2e7a7ccc1ff85db
YaelAdair11 <yaelgtz11@gmail.com>
2025-11-06 13:08:11 -0600

    svelte

---
+++ svelte-app/src/Hijo.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let texto = "boton";
+    export let color = "blue";
+    export const nombre = "Svelte";
+</script>
+
+<main>
+<button style="background-color: {color};">
+    {texto}
+</button>
+</main>
+
+<style>
+    button {
+        color: white;
+        border: none;
+        padding: 10px 20px;
+        font-size: 16px;
+        border-radius: 5px;
+        cursor: pointer;
+    }
+</style>
